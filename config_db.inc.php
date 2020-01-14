f82cce90087fc2699219b36aad20287e04fc5cae
Roland Stens <roland.stens@gmail.com>
2020-01-14 12:07:30 -0800

    db config

--- config_db.inc.php
+++ config_db.inc.php
@@ -1,6 +1,8 @@
-define('DB_TYPE', trim(getenv('DB_TYPE'));
-define('DB_USER', trim(getenv('DB_USER'));
-define('DB_PASS', trim(getenv('DB_PASS'));
-define('DB_HOST', trim(getenv('DB_HOST'));
-define('DB_NAME', trim(getenv('DB_NAME'));
-define('DB_TABLE_PREFIX', trim(getenv('DB_TABLE_PREFIX'));
+<?php
+    define('DB_TYPE', trim(getenv('DB_TYPE'));
+    define('DB_USER', trim(getenv('DB_USER'));
+    define('DB_PASS', trim(getenv('DB_PASS'));
+    define('DB_HOST', trim(getenv('DB_HOST'));
+    define('DB_NAME', trim(getenv('DB_NAME'));
+    define('DB_TABLE_PREFIX', trim(getenv('DB_TABLE_PREFIX'));
+?>    
